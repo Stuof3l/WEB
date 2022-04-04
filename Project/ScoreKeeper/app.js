337503bd5fdfcfca5d4a19bfd5d05e39c3504298
Stuof3l <zehuaw@andrew.cmu.edu>
2022-04-04 14:59:49 -0400

    refactor

--- Project/ScoreKeeper/app.js
+++ Project/ScoreKeeper/app.js
@@ -16,38 +16,27 @@ const winningScoreSelect = document.querySelector("#playto");
 let winningScore = 3;
 let isGameOver = false;
 
-function updateScore() {
-    
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        player.display.textContent = player.score;
+        if(player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add("has-text-success");
+            opponent.display.classList.add("has-text-danger");
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+    }
 }
 
-p1Button.addEventListener('click', function(e){
-    // if not end
-    if (!isGameOver) {
-        p1Score += 1;
-        p1Display.textContent = p1Score;
-        if(p1Score === winningScore) {
-            isGameOver = true;
-            p1Display.classList.add("has-text-success");
-            p2Display.classList.add("has-text-danger");
-            p1Button.disabled = true;
-            p2Button.disabled = true;
-        }
-    }
+p1.button.addEventListener('click', function(e){
+    updateScores(p1, p2);
+    
 })
 
-p2Button.addEventListener('click', function(e){
-    // if not end
-    if (!isGameOver) {
-        p2Score += 1;
-        p2Display.textContent = p2Score;
-        if(p2Score === winningScore) {
-            isGameOver = true;
-            p2Display.classList.add("has-text-success");
-            p1Display.classList.add("has-text-danger");
-            p1Button.disabled = true;
-            p2Button.disabled = true;
-        }
-    }
+p2.button.addEventListener('click', function(e){
+    updateScores(p2, p1);
 })
 
 resetButton.addEventListener('click', reset);
@@ -59,12 +48,12 @@ winningScoreSelect.addEventListener("change", function(e){
 
 function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = p1Score;
-    p2Display.textContent = p2Score;
-    p1Display.classList.remove("has-text-success", "has-text-danger")
-    p2Display.classList.remove("has-text-success", "has-text-danger")
-    p1Button.disabled = false;
-    p2Button.disabled = false;
+    p1.score = 0;
+    p2.score = 0;
+    p1.display.textContent = p1.score;
+    p2.display.textContent = p2.score;
+    p1.display.classList.remove("has-text-success", "has-text-danger")
+    p2.display.classList.remove("has-text-success", "has-text-danger")
+    p1.button.disabled = false;
+    p2.button.disabled = false;
 }
