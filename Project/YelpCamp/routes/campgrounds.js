93a6bd3bf00f4e91da5f0d08c4cfc77bfa5ecf5e
Stuof3l <zehuaw@andrew.cmu.edu>
2022-06-12 22:38:42 -0400

    session

--- Project/YelpCamp/routes/campgrounds.js
+++ Project/YelpCamp/routes/campgrounds.js
@@ -9,8 +9,8 @@ const { campgroundSchema,} = require('../schemas.js');
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
     if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new expressError(msg, 400)
+        const msg = error.details.map(el => el.message).join(',');
+        throw new expressError(msg, 400);
     } else {
         next();
     }
