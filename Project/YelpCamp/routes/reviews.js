539dc03e0d796035dac9857504ea7b203c97bd84
Stuof3l <zehuaw@andrew.cmu.edu>
2022-06-29 17:29:09 -0400

    refactor 3

--- Project/YelpCamp/routes/reviews.js
+++ Project/YelpCamp/routes/reviews.js
@@ -8,7 +8,6 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const expressError = require('../utils/expressError');
 const catchAsync = require('../utils/catchAsync');
 
-
 router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
