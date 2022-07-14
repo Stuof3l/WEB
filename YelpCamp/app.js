1cb8674ad3939faf1c7058e8f3df79449ff5c510
Stuof3l <zehuaw@andrew.cmu.edu>
2022-07-14 18:53:43 -0400

    add start script

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -199,6 +199,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Serving ON PORT 3000!");
-});
+// the port is different on Heroku
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
