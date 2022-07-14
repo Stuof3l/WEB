ab337b1037cc84473c36c136e7cf18cd402baffc
Stuof3l <zehuaw@andrew.cmu.edu>
2022-07-14 19:21:03 -0400

    deploy

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -32,7 +32,7 @@ const MongoStore = require('connect-mongo');
 
 // MongoDB Cloud
 // const dbUrl = process.env.DB_URL;
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+const dbUrl = "mongodb://localhost:27017/yelp-camp" || process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
